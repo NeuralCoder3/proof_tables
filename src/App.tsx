a0af09656b47146cd683b77bf7774ea806826fb6
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2023-09-24 21:42:09 +0200

    test of jscoq

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// @ts-ignore
+// import { JsCoq } from './jscoq/dist/frontend/index';
+
+
+import { JsCoq, CoqManager } from 'jscoq';
+    // import { JsCoq } from './node_modules/jscoq/jscoq.js';
+
+    // var jscoq_ids : string[] = [];
+    // var jscoq_opts = {
+    //     prelude:       true,
+    //     implicit_libs: true,
+    //     editor:        { mode: { 'company-coq': true }, keyMap: 'default' },
+    //     init_pkgs:     ['init'],
+    //     all_pkgs:      ['coq']
+    // };
+
+    // const hidden_textarea = document.createElement('textarea');
+    // hidden_textarea.id = 'area';
+
+    // JsCoq.start(jscoq_ids, jscoq_opts);
+    // JsCoq.load().then(() => {
+    //   console.log("JsCoq loaded");
+    // });
+    // JsCoq.start(['code']).then(() => {
+    //   console.log("JsCoq started");
+    // });
+    // console.log(coq);
+
+// const coq = new CoqManager([]);
+// const coq = new CoqWorker(null,null,"js",true);
+
+// console.log(coq);
 
 function App() {
+
+  // const coq = JsCoq.start();
+  // const coq = new CoqManager([],
+  //   {
+  //     base_path: "../../",
+  //   }
+  //   );
+
   return (
     <div className="App">
       <header className="App-header">
